4d05cdfce08c6ed1e55857e89b307629bb34256e
carlo denaro <blackout314@auburn.dreamhost.com>
2019-03-28 06:52:58 -0700

    test try

---
+++ Tests/BotTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class BotTest extends PHPUnit_Framework_TestCase
+{
+  public function setUp()
+  {
+  }
+
+  public function tearDown()
+  {
+  }
+
+  public function testHelloWorld()
+  {
+    $this->assertEquals('Hello World', 'Hello World');
+  }
+
+}
+
+
