5fdd158867b6e2ec409d18f86d16b5854f968287
Carlo Denaro <818568+blackout314@users.noreply.github.com>
2019-03-29 10:21:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
+
 require 'config.php';
-
-define('API_KEY',     $TOKEN);
-
+define('API_KEY',   $TOKEN);
+define('DEBUG',     false);  
 require 'utils.php';
-
 
 $data   = file_get_contents("php://input");
 $update = json_decode($data, true);
 
-logThis(999, json_encode($update));
+if(DEBUG) {
+    logThis(999, json_encode($update));
+}
 
 $new    = $update['message']['new_chat_members'];
 $left   = $update['message']['left_chat_member'];
 
 $msg    = $update['message']['text'];
+$chat_id = $update['message']['from']['id'];
 
 if($msg == "/start" || $msg == "/help"){
-    $chat_id = $update['message']['from']['id'];
-
     makeHTTPRequest('sendMessage',[
         'chat_id'   => $update['message']['from']['id'],
         'text'      => "!zbalance <i>address</i>\n!ztx <i>address</i> last 10 tx",
@@ -27,7 +27,6 @@ if($msg == "/start" || $msg == "/help"){
 }
 
 if(preg_match('/^!zbalance(\s)+([0-9a-z])+/i', $msg)) {
-    $chat_id = $update['message']['from']['id'];
     $addr = trim(str_replace('!zbalance','',$msg));
 
     makeHTTPRequest('sendMessage',[
@@ -37,8 +36,7 @@ if(preg_match('/^!zbalance(\s)+([0-9a-z])+/i', $msg)) {
     ]);
 }
 
-if(preg_match('/^!ztx(\s)+([0-9a-z])+/i', $msg)) {
-    $chat_id = $update['message']['from']['id'];
+if(preg_match('/^!ztx(\s)+([0-9a-z])+/i', $msg)) {   
     $addr = trim(str_replace('!ztx','',$msg));
 
     makeHTTPRequest('sendMessage',[
